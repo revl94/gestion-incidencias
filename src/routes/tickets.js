513520120e4966ffb4947f06ebe661cd3961325a
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-07-31 09:29:10 -0400

    Connect to clockify

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 const { promisify } = require('util');
+const axios = require('axios')
 //Project's own requires
 const database  = require('../../config/config');
+const pool = require('../../database');
+const workspaceId = '5c79aee1b079877a63415e08'
+//Initializations
 const glpi = mysql.createPool(database.glpi);
 
 glpi.getConnection((err, connection) => {
@@ -11,8 +15,13 @@ glpi.getConnection((err, connection) => {
     return;
 });
 glpi.query = promisify(glpi.query);
-const pool = require('../../database');
-//Initializations
+
+const ClockifyAxios = axios.create({
+    baseURL: 'https://api.clockify.me/api/v1'
+});
+ClockifyAxios.defaults.headers.common['Content-Type'] = 'application/json';
+ClockifyAxios.defaults.headers.common['X-Api-Key'] = 'Xvy1392jqzm2LxBF';
+
 const router = express.Router();
 const HttpStatus = require('http-status-codes');
 
@@ -30,47 +39,136 @@ router.get('/get_ticket/:id', async (req, res) => {
             "tic_closing_date": result[0]["Fecha Cierre"], "tic_sol_date": result[0]["Fecha Solución"] })
     }else{
         await pool.query('UPDATE tickets SET tic_title = ?, tic_description = ?, tic_branch = ?, tic_subsidiary = ?, tic_deparment = ?, tic_usr_ci = ?, tic_category = ?, tic_priority = ?, tic_assigned_to = ?, tic_date = ?, tic_last_update_date = ?, tic_closing_date = ?, tic_sol_date = ?  WHERE tic_id = ?', 
-        [ result[0]["Título"], result[0]["Descripción"], result[0]["Ramo"], result[0]["Sucursal"], result[0]["Departamento"], result[0]["Username"], result[0]["Categoría"], result[0]["Prioridad"], result[0]["Asignado a"], result[0]["Fecha Solicitud"], ["Fecha Último Cambio"], result[0]["Fecha Cierre"], result[0]["Fecha Solución"], result[0]["Id Ticket"] ])
+        [ result[0]["Título"], result[0]["Descripción"], result[0]["Ramo"], result[0]["Sucursal"], result[0]["Departamento"], result[0]["Username"], result[0]["Categoría"], result[0]["Prioridad"], result[0]["Asignado a"], result[0]["Fecha Solicitud"], result[0]["Fecha Último Cambio"], result[0]["Fecha Cierre"], result[0]["Fecha Solución"], result[0]["Id Ticket"] ])
     }
     res.json({glpi: result})
 });
 
-router.get('/get_tickets', async (req, res) => {
-    try {
-        await clockifyHours(51);
-        const ticket = await pool.query(`SELECT * FROM tickets
-        ORDER BY tickets.tic_title ASC`);
-        if (ticket.length > 0 ) {
-            type= 'success';
-            console.log(ticket);
-            res.status(HttpStatus.OK).json({ticket, type});
-        } else {
-            type="Not Data";
-            res.status(HttpStatus.OK).json({ticket, type});
+router.get('/get_hours/:id', async (req, res) => {
+    const { id } = req.params
+    const ticket = await pool.query('SELECT * FROM tickets WHERE `tic_id` = '+id)
+    if(ticket.length == 0){
+        res.send("ERROR")
+    }else{
+        const user =  await pool.query('SELECT * FROM user WHERE `usr_ci` = '+ticket[0].tic_usr_ci);
+        if(user.length == 0){
+            res.send("ERROR")
+        }else{
+            const userId = await getUserID(user[0].usr_email);
+            const hours = await getTimeEntries(userId, ticket[0].tic_title)
+            if(hours.length > 0){
+                let time = {}
+                let min = 0
+                let totalmin = 0
+                let totalhours = 0
+                hours.forEach(async (dat, i) => {
+                    oldFormat = hours[i].timeInterval.duration
+                    newFormat = oldFormat.split('PT')
+                    isH = checkH(newFormat[1])
+                    isM = checkM(newFormat[1])
+                    isS = checkS(newFormat[1])
+                    timeFormatted = formatTime(isH, isM, isS)
+                    time[i] = {
+                        "description": hours[i].description,
+                        "hours": timeFormatted[0],
+                        "minutes": timeFormatted[1],
+                        "seconds": timeFormatted[2]
+                    }
+                    min = (parseInt(timeFormatted[0])*60) + parseInt(timeFormatted[1])
+                    totalmin += min
+                })
+                totalhours = totalmin / 60
+                const mins = (totalhours-Math.floor(totalhours))*60
+                const secs = (mins-Math.floor(mins))*60
+                const finaltime = Math.floor(totalhours) + ":" + Math.floor(mins) + ":" + Math.floor(secs)
+                await pool.query('UPDATE tickets SET tic_clockify_time = ?  WHERE tic_id = ?', 
+                [finaltime , id])
+                res.json({finaltime})
+            }
         }
-
-    } catch(err) {
-        printError(err);
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({err})
     }
 });
 
+function getUserID(email) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await ClockifyAxios.get(
+          `/workspaces/${workspaceId}/users?email=${email}`
+        );
+        resolve(result.data[0].id);
+      } catch (error) {
+        printError(error)
+        reject(error);
+      }
+    });
+  }
 
-async function clockifyHours(reqId) {
+function getTimeEntries(userId, descp) {
 
-    const cloHours = await pool.query(`SELECT act_time_loaded FROM activities 
-            WHERE activities.req_id = '${reqId}'`);
-    let totalHours = 0;
-    for (let i = 0; i <= cloHours.length; i++) {
-        totalHours+=cloHours[i];
-    }
-
-    console.log(totalHours);
-    console.log(cloHours.length);
-
-
+    return new Promise(async (resolve,reject) => {
+        try {
+            const params = {
+                description: descp,
+            }
+            const result = await ClockifyAxios.get(`/workspaces/${workspaceId}/user/${userId}/time-entries`, {params})
+            resolve(result.data)
+        } catch (error) {
+            printError(error)
+            reject(error)
+        }
+      });
 }
+function checkH(duration) {
+    return duration.indexOf("H");
+}
+  
+function checkM(duration) {
+    return duration.indexOf("M");
+}
+  
+function checkS(duration) {
+    return duration.indexOf("S");
+}
+function formatTime(isH, isM, isS) {
+    hours= 0
+    seconds= 0
+    minutes= 0
+    if(isH > 0 && isM > 0 && isS > 0){
+        splitH = newFormat[1].split('H')
+        hours = splitH[0]
+        splitM = splitH[1].split('M')
+        minutes = splitM[0]
+        splitS = splitM[1].split('S')
+        seconds = splitS[0]
+    }else if (isH > 0 && isM > 0 && isS < 0) {
+        splitH = newFormat[1].split('H')
+        hours = splitH[0]
+        splitM = splitH[1].split('M')
+        minutes = splitM[0]
+    }else if (isH > 0 && isS > 0 && isM < 0){
+        splitH = newFormat[1].split('H')
+        hours = splitH[0]
+        splitS = splitH[1].split('S')
+        seconds = splitS[0]
+    }else if (isM > 0 && isS > 0 && isH < 0){
+        splitM = newFormat[1].split('M')
+        minutes = splitM[0]
+        splitS = splitM[1].split('S')
+        seconds = splitS[0]
+    }else if( isH > 0 && isM < 0 && isS < 0){
+        splitH = newFormat[1].split('H')
+        hours = splitH[0]
+    }else if( isH == -1 && isM >= 0 && isS == -1){
 
+        splitM = newFormat[1].split('M')
+        minutes = splitM[0]
+    }else if( isH < 0 && isM < 0 && isS > 0){
+        splitS = newFormat[1].split('S')
+        seconds = splitS[0]
+    }
+    let time = [hours, minutes, seconds]
+    return time
+}
 //Funcion para imprimir errores
 function printError(e){
     if(e!=null){
