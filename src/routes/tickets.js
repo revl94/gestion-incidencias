dc15b0ea47a02c280f68863f83c435d5e2beab43
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-08-27 08:10:46 -0400

    Update tickets.js

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -340,7 +340,7 @@ async function fillAllData(){
 const interval_long =  60*60*1000;//1 hora
 async function timer(interval_long){
     const date = (new Date(new Date().toLocaleString("en-US", {timeZone: "America/Caracas"}))).getHours(); // Create a Date object to find out what time it is
-    if((date < 13 && date > 12) || (date < 19 && date > 18) ){ // Check the time at 12:00PM - 01:00PM OR 06:00PM - 07:00PM
+    if( (date < 13 && date >= 12) || (date < 19 && date >= 18) ){ // Check the time at 12:00PM - 01:00PM OR 06:00PM - 07:00PM
         await fillAllData()
     }
     setTimeout(function(){ timer(interval_long); }, interval_long);//Renew timer
